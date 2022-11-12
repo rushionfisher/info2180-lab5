ff920a8979814f5f57b099baf38f6980a57483a2
rushionfisher <rushionfisher@gmail.com>
2022-11-11 19:10:26 -0500

    Final code for exercise 5

--- world.js
+++ world.js
@@ -1,31 +1,41 @@
 window.onload = function() {
 
-    const  input = document.getElementById('country');
-    const  result = document.getElementById('result');
-    const  look = document.getElementById('lookup');
-    const  httpReq= new XMLHttpRequest();
-    const REG=(url)=>{
-      httpReq.onreadystatechange = callback;
-      httpReq.open('GET', url);
-      httpReq.send();
-    }
-    look.addEventListener('click', function(event) {
-      event.preventDefault();
-      const searchText = input.value;
-      let stored = searchText.trim();
-      const  url = `world.php?country=${stored}`;
-      REG(url);
-      console.log(stored)
-    });
-  
-    function callback() {
-      if (httpReq.readyState === XMLHttpRequest.DONE) {
-        if (httpReq.status === 200) {
-          const res = httpReq.responseText;
-          result.innerHTML=res;
-        } else {
-          alert('Unfortunately, there was a problem with the request.');
-        }
-      }
-    }  
+  const  input = document.getElementById('country');
+  const  result = document.getElementById('result');
+  const  lookup = document.getElementById('lookup');
+  const  city = document.getElementById('cities');
+  const  httpReq= new XMLHttpRequest();
+   
+  const REG=(url)=>{
+	httpReq.onreadystatechange = callback;
+	httpReq.open('GET', url);
+	httpReq.send();
   }
+    lookup.addEventListener('click', function(event) {
+    event.preventDefault();
+    const searchText = input.value;
+    let stored = searchText.trim();
+    const  url = `world.php?country=${stored}`;
+    REG(url);
+  });
+
+    city.addEventListener('click', function(event) {
+    event.preventDefault();
+    const searchText = input.value;
+    let save = searchText.trim();
+    const  url = `world.php?country=${save}&context=${save}`;
+    REG(url);
+  });
+
+  const callback=()=> {
+    input.value = '';
+    if (httpReq.readyState === XMLHttpRequest.DONE) {
+      if (httpReq.status === 200) {
+        const res = httpReq.responseText;
+		result.innerHTML=res;
+      } else {
+        alert('Unfortunately, there was a problem with the request.');
+      }
+    }
+  }
+}
--- world.php
+++ world.php
@@ -9,17 +9,17 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$pcountry=trim(filter_var(htmlspecialchars($_GET['country']), FILTER_SANITIZE_STRING));
+$country= $conn->query("SELECT * FROM countries WHERE name LIKE '%$pcountry%'");
+$countryque= $country->fetchAll(PDO::FETCH_ASSOC);
 
-
-$reqCountry=trim(filter_var(htmlspecialchars($_GET['country']), FILTER_SANITIZE_STRING));
-$country= $conn->query("SELECT * FROM countries WHERE name LIKE '%$reqCountry%'");
-$countryQ= $country->fetchAll(PDO::FETCH_ASSOC);
-
-
-
+$pcity =trim(filter_var(htmlspecialchars($_GET['context']), FILTER_SANITIZE_STRING)); 
+$city= $conn->query("SELECT cities.population, cities.district ,cities.name FROM cities JOIN countries ON countries.code=cities.country_code WHERE countries.name LIKE '%$pcity%'");
+$cityque= $city->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<?php if (isset($_GET['country']) && !isset($_GET['context'])):  ?>
+
+  <?php if (isset($_GET['country']) && !isset($_GET['context'])):  ?>
     <table>
         <tr>
           <th> Country Name</th>  
@@ -29,14 +29,33 @@ $countryQ= $country->fetchAll(PDO::FETCH_ASSOC);
         </tr>
         
         <tbody>
-        <?php foreach ($countryQ as $place): ?>
+        <?php foreach ($countryque as $area): ?>
             <tr>
-                <td> <?= $place['name']; ?></td>  
-                <td> <?= $place['continent']; ?></td>  
-                <td> <?= $place['independence_year']; ?></td>  
-                <td> <?= $place['head_of_state']; ?></td>  
+                <td> <?= $area['name']; ?></td>  
+                <td> <?= $area['continent']; ?></td>  
+                <td> <?= $area['independence_year']; ?></td>  
+                <td> <?= $area['head_of_state']; ?></td>  
             </tr>
          <?php endforeach; ?>
         </tbody>
     </table>
-    <?php endif ?>
+        
+    <?php elseif (isset($_GET['context']) && isset($_GET['country'])):?>
+        <table>
+            <tr>
+              <th> Name</th>  
+              <th> District</th>  
+              <th> Popululation</th>  
+            </tr>
+            
+            <tbody>
+            <?php foreach ($cityque as $place): ?>
+                <tr>
+                    <td> <?= $place['name']; ?></td>  
+                    <td> <?= $place['district']; ?></td>  
+                    <td> <?= $place['population']; ?></td>  
+                </tr>
+             <?php endforeach; ?>
+            </tbody>
+        </table>
+<?php endif ?>
